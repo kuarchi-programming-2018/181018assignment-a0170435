ac7c9255fd515a0e25ca25d1dfa347d297d4a08a
a0170435 <43874692+a0170435@users.noreply.github.com>
2018-10-13 20:13:50 +0900

    課題に取り組みました。

---
+++ Paiza/5-04-3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+numbers=[[7 for i in range(4)] for j in range(5)]
+print(numbers)
+
+
---
+++ Paiza/6-06-2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+class Gakusei:
+    def __init__(self, kokugo, sansu):
+        self.kokugo = kokugo
+        self.sansu  = sansu
+
+    # この下に、合計得点を戻り値として返すsumメソッドを記述する
+    def sum(self):
+        return str(self.kokugo+self.sansu)
+        
+yamada = Gakusei(70, 43)
+print("合計は" + str(yamada.sum()) + "点です")
+
+
+
